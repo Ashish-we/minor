22a765d9d56e30a691bc0a2eb5eb8b51aebf0c46
Ashish-we <acharya.seven@gmail.com>
2023-05-21 15:25:22 +0545

    draft

---
+++ client-main/frontedn/src/components/companyDashboard.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+
+const CompanyDashboard = () => {
+
+
+    return (
+        <>
+        <h1>Hello guys</h1>
+        
+        </>
+    )
+}
+
+export default CompanyDashboard;
